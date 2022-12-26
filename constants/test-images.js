a18b5970d2023426611f294abcd2e6c47c984bb5
Rahultadmybrand <rahul.thakur@admybrand.com>
2022-12-26 22:51:59 +0530

    test page

---
+++ constants/test-images.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    heading: "Most Necessary",
+    subheading: "Eminem",
+    backgroundColor: "black",
+    backgroundImage:
+      "https://images.unsplash.com/photo-1619622637662-0104aa03a4af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFwfGVufDB8MnwwfHw=&auto=format&fit=crop&w=500&q=60",
+    color: "yellow",
+  },
+  {
+    heading: "Acoustic Love",
+    subheading: "Ed Sheeran",
+    backgroundColor: "black",
+    backgroundImage:
+      "https://images.unsplash.com/photo-1620829912031-0a8af72c55d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWNvdXN0aWN8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    color: "pink",
+  },
+  {
+    heading: "Vigilante Ctx",
+    subheading: "Taylor Swift",
+    backgroundColor: "black",
+    backgroundImage:
+      "https://images.unsplash.com/photo-1608889175157-718b6205a50d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmF0bWFufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    color: "red",
+  },
+  {
+    heading: "DNA Ctx",
+    subheading: "Kendrick Lamar",
+    backgroundColor: "black",
+    backgroundImage:
+      "https://images.unsplash.com/photo-1534050055340-71c7fa612a99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHJhcHxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60",
+    color: "#565656",
+  },
+  {
+    heading: "DNA Ctx",
+    subheading: "Kendrick Lamar",
+    backgroundColor: "black",
+    backgroundImage:
+      "https://images.unsplash.com/photo-1534050055340-71c7fa612a99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHJhcHxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60",
+    color: "#565656",
+  },
+  {
+    heading: "DNA Ctx",
+    subheading: "Kendrick Lamar",
+    backgroundColor: "black",
+    backgroundImage:
+      "https://images.unsplash.com/photo-1534050055340-71c7fa612a99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHJhcHxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60",
+    color: "#565656",
+  },
+];
